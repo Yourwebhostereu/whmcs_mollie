5391f187f3c7fa2b1003ab5b9dbbfdf379cd41e4
daniel.koop <daniel@yourwebhoster.eu>
2024-08-30 14:30:17 +0200

    Refactor Mollie API integration.
    Updated function calls to match new SDK structure. Removed redundant closing PHP tags.

--- Mollie/callback.php
+++ Mollie/callback.php
@@ -10,76 +10,75 @@ $gatewaymodule = "Mollie";// Enter your gateway module name here replacing templ
 
 $GATEWAY = getGatewayVariables($gatewaymodule);
 if (!$GATEWAY["type"])
-	die("Module Not Activated"); // Checks gateway module is active before accepting callback
+    die("Module Not Activated"); // Checks gateway module is active before accepting callback
 
 try
 {
-	/*
-	 * Initialize the Mollie API library with your API key.
-	 *
-	 * See: https://www.mollie.nl/beheer/account/profielen/
-	 */
-	if($GATEWAY['testmode'] == 'on')
-		$apiKey = $GATEWAY['MollieTestAPIKey'];
-	else
-		$apiKey = $GATEWAY['MollieLiveAPIKey'];
+    /*
+     * Initialize the Mollie API library with your API key.
+     *
+     * See: https://www.mollie.nl/beheer/account/profielen/
+     */
+    if($GATEWAY['testmode'] == 'on')
+        $apiKey = $GATEWAY['MollieTestAPIKey'];
+    else
+        $apiKey = $GATEWAY['MollieLiveAPIKey'];
 
-	/*
-	 * Initialize the Mollie API library with your API key.
-	 *
-	 * See: https://www.mollie.nl/beheer/account/profielen/
-	 */
-	$mollie = new \Mollie\Api\MollieApiClient();
-	$mollie->setApiKey($apiKey);
+    /*
+     * Initialize the Mollie API library with your API key.
+     *
+     * See: https://www.mollie.nl/beheer/account/profielen/
+     */
+    $mollie = new \Mollie\Api\MollieApiClient();
+    $mollie->setApiKey($apiKey);
 
-	/*
-	 * Retrieve the payment's current state.
-	 */
-	$payment  	= $mollie->payments->get($_REQUEST['id']);
-	$fee 		= '0.00';
+    /*
+     * Retrieve the payment's current state.
+     */
+    $payment  	= $mollie->payments->get($_REQUEST['id']);
+    $fee 		= '0.00';
 
-	$invoiceId = checkCbInvoiceID($payment->metadata->invoiceId, $GATEWAY["name"]); // Checks invoice ID is a valid invoice number or ends processing
+    $invoiceId = checkCbInvoiceID($payment->metadata->invoiceId, $GATEWAY["name"]); // Checks invoice ID is a valid invoice number or ends processing
 
-	checkCbTransID($payment->id); // Checks transaction number isn't already in the database and ends processing if it does
+    checkCbTransID($payment->id); // Checks transaction number isn't already in the database and ends processing if it does
 
 
-	if ($payment->isPaid() == TRUE)
-	{
-	    // The payment was successful
-	    addInvoicePayment($invoiceId, $payment->id, $payment->amount->value, $fee, $gatewaymodule); # Apply Payment to Invoice: invoiceId, transactionid, amount paid, fees, modulename
-		logTransaction($GATEWAY["name"], $_POST, "Successful"); # Save to Gateway Log: name, data array, status
-	}
-	elseif ($payment->isOpen() == FALSE)
-	{
-		// The payment was unsuccesful
-		logTransaction($GATEWAY["name"], $_POST, "Unsuccessful"); # Save to Gateway Log: name, data array, status
-	}
+    if ($payment->isPaid() == TRUE)
+    {
+        // The payment was successful
+        addInvoicePayment($invoiceId, $payment->id, $payment->amount->value, $fee, $gatewaymodule); # Apply Payment to Invoice: invoiceId, transactionid, amount paid, fees, modulename
+        logTransaction($GATEWAY["name"], $_POST, "Successful"); # Save to Gateway Log: name, data array, status
+    }
+    elseif ($payment->isOpen() == FALSE)
+    {
+        // The payment was unsuccesful
+        logTransaction($GATEWAY["name"], $_POST, "Unsuccessful"); # Save to Gateway Log: name, data array, status
+    }
 }
 catch (Mollie_API_Exception $e)
 {
-	// Something went wrong and by not returning a 200 header we let Mollie try it again later.
-	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    // Something went wrong and by not returning a 200 header we let Mollie try it again later.
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 
-	$responseData['id']						=	@$payment->id;
-	$responseData['mode']					=	@$payment->mode;
-	$responseData['createdDatetime']		=	@$payment->createdDatetimed;
-	$responseData['status']					=	@$payment->status;
-	$responseData['paidDatetime']			=	@$payment->paidDatetime;
-	$responseData['cancelledDatetime']		=	@$payment->cancelledDatetime;
-	$responseData['expiredDatetime']		=	@$payment->expiredDatetime;
-	$responseData['expiryPeriod']			=	@$payment->expiryPeriod;
-	$responseData['amount']					=	@$payment->amount->value;
-	$responseData['description']			=	@$payment->description;
-	$responseData['method']					=	@$payment->method;
-	$responseData['metadata']				=	@$payment->metadata;
-	$responseData['locale']					=	@$payment->locale;
-	$responseData['details']				=	@$payment->details;
-	$responseData['links']					=	@$payment->links;
-	$responseData['locale']					=	@$payment->locale;
-	$responseData['locale']					=	@$payment->locale;
-	$responseData['exceptionMessage']		=	$e->getMessage();
+    $responseData['id']						=	@$payment->id;
+    $responseData['mode']					=	@$payment->mode;
+    $responseData['createdDatetime']		=	@$payment->createdDatetimed;
+    $responseData['status']					=	@$payment->status;
+    $responseData['paidDatetime']			=	@$payment->paidDatetime;
+    $responseData['cancelledDatetime']		=	@$payment->cancelledDatetime;
+    $responseData['expiredDatetime']		=	@$payment->expiredDatetime;
+    $responseData['expiryPeriod']			=	@$payment->expiryPeriod;
+    $responseData['amount']					=	@$payment->amount->value;
+    $responseData['description']			=	@$payment->description;
+    $responseData['method']					=	@$payment->method;
+    $responseData['metadata']				=	@$payment->metadata;
+    $responseData['locale']					=	@$payment->locale;
+    $responseData['details']				=	@$payment->details;
+    $responseData['links']					=	@$payment->links;
+    $responseData['locale']					=	@$payment->locale;
+    $responseData['locale']					=	@$payment->locale;
+    $responseData['exceptionMessage']		=	$e->getMessage();
 
-	logModuleCall($gatewaymodule, 'Mollie Callback action', $_POST['id'], serialize($responseData), '', '');
+    logModuleCall($gatewaymodule, 'Mollie Callback action', $_POST['id'], serialize($responseData), '', '');
 }
 
-?>
--- README.md
+++ README.md
@@ -4,8 +4,5 @@
 ## Installation
 Download the zip and install the files to modules/gateways/
 
-## Features
-
-- Every transaction cost is recorded and can be set to custom transaction cost’s.
-- Customers choose the Mollie gateway, which you can configure with your own name. At the Mollie website the client can choose the desired payment method.
-
+## Feature
+Customers choose the Mollie gateway, which you can configure with your own name. At the Mollie website the client can choose the desired payment method.
